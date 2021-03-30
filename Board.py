4b1d526c934d14ca3f7a4ad0bb88ba16b904cd88
ErTitoKevin <linden_kevin@web.de>
2021-03-30 13:34:11 +0200

    Changed the return message, when the game is a Tie. Created a winning_combo list that has all possible winning combos.

--- Board.py
+++ Board.py
@@ -4,6 +4,9 @@ blank = "-"
 board = {"a_1": blank, "b_1": blank, "c_1": blank,
          "a_2": blank, "b_2": blank, "c_2": blank,
          "a_3": blank, "b_3": blank, "c_3": blank }
+winning_combos =[["a_1", "a_2", "a_3"],["b_1", "b_2", "b_3"],["c_1", "c_2", "c_3"],
+                 ["a_1", "b_1", "c_1"],["a_2", "b_2", "c_2"],["a_3", "b_3", "c_3"],
+                 ["a_1", "b_2", "c_3"],["a_3", "b_2", "c_1"]]
 board_template = """
                                        a     b     c
                                           |     |     
@@ -22,9 +25,8 @@ def make_board_string():
     return board_string
 #Reads the players input
 def read_choice():
-    user_choosing = True
     choice = input("Choose your move: ")
-    while user_choosing:
+    while True:
         coord = choice[:3]  # Board Key
         x_or_o = choice[4:5]  # X or O choice
         if coord in board and x_or_o in (x, o):
@@ -33,13 +35,18 @@ def read_choice():
             choice = input("Your input was not valid, please try again: ")
 #Places the chosen piece into the make_board_string
 def place_piece():
-    game_on = True
-    while game_on:
+    while True:
         coordinate, piece = read_choice()
         board[coordinate] = piece
         print(make_board_string())
         if blank not in board.values():
-            return "The gameboard is full"
+            return "The Game is a Tie!"
 
 
 print(place_piece())
+
+for winning_comb in winning_combos:
+    for comb in winning_comb:
+        if comb in coordinate:
+            winner = piece + " has won Tic_Tac_toe!!"
+            return winner
